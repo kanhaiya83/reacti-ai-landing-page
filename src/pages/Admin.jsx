e1f5f81623de35641cddf39aa9477c61c5f9b7ce
kanhaiya <kanhaiya2778@gmail.com>
2023-03-06 21:13:48 +0530

    add custom prompt form

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -3,9 +3,11 @@ import { useEffect } from "react";
 import CodeForm from "../components/CodeForm";
 import Header from "../components/Header";
 import { downloadExcel } from "../utils";
+import { successToast } from "../utils/notify";
 
 const AdminPage = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [customPrompt, setCustomPrompt] = useState("dijvwhviuew");
   const [couponsData, setCouponsData] = useState([]);
   useEffect(() => {
     if (localStorage.getItem("isAdminLoggedIn")) {
@@ -22,10 +24,18 @@ const AdminPage = () => {
       if (response.success) {
         let dataArr = [];
         for (const key in response.data) {
-          dataArr.push({...response.data[key],key});
+          dataArr.push({ ...response.data[key], key });
         }
         console.log(dataArr);
         setCouponsData(dataArr);
+      }
+    })();(async () => {
+      const res = await fetch(
+        import.meta.env.VITE_SERVER_URL + "/admin/prompt"
+      );
+      const response = await res.json();
+      if (response.success) {
+          setCustomPrompt(response.prompt)        
       }
     })();
   }, [isLoggedIn]);
@@ -39,7 +49,15 @@ const AdminPage = () => {
   };
 
   const content = isLoggedIn ? (
-    <AdminPanel couponsData={couponsData} setCouponsData={setCouponsData}/>
+    <AdminPanel
+      _hooks={{
+        couponsData,
+        setCouponsData,
+        setIsLoggedIn,
+        customPrompt,
+        setCustomPrompt,
+      }}
+    />
   ) : (
     <LoginScreen handleSubmit={handleSubmit} />
   );
@@ -63,9 +81,32 @@ const LoginScreen = ({ handleSubmit }) => {
     </div>
   );
 };
-const AdminPanel = ({ setIsLoggedIn, couponsData ,setCouponsData}) => {
+const AdminPanel = ({ _hooks }) => {
+  const {
+    couponsData,
+    setCouponsData,
+    setIsLoggedIn,
+    customPrompt,
+    setCustomPrompt,
+  } = _hooks;
   const [modalIsOpen, setIsOpen] = useState(false);
-
+  const handlePromptChange = (e) => {
+    setCustomPrompt(e.target.value);
+  };
+  const handlePromptSave = async() => {
+    if (!customPrompt.includes("<tone>") || !customPrompt.includes("<tweet>")) {
+      alert("Invalid syntax");
+    }
+      const res = await fetch(
+        import.meta.env.VITE_SERVER_URL + "/admin/prompt",
+        {method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({prompt:customPrompt})}
+      );
+      const response = await res.json();
+      if (response.success) {
+       successToast("Prompt changed successfully!")
+      }
+    }
+  
   return (
     <div className="w-full px-[5%] py-8">
       <div className="w-full flex justify-end">
@@ -89,7 +130,7 @@ const AdminPanel = ({ setIsLoggedIn, couponsData ,setCouponsData}) => {
           Generate Codes
         </button>
       </div>
-      <div className="grid grid-cols-[repeat(24,1fr)] w-full bg-slate-900 text-center mt-10">
+      <div className="grid grid-cols-[repeat(24,1fr)] w-full bg-slate-900 text-center mt-10 max-h-[600px] overflow-scroll ">
         <div className=" col-span-1 p-2">
           <h1 className="text-xl ">S No</h1>
         </div>
@@ -115,15 +156,32 @@ const AdminPanel = ({ setIsLoggedIn, couponsData ,setCouponsData}) => {
           <button></button>
         </div>
         {couponsData.map((c, i) => {
-          return <RowItem data={c} index={i} key={i} setCouponsData={setCouponsData}/>;
+          return (
+            <RowItem
+              data={c}
+              index={i}
+              key={i}
+              setCouponsData={setCouponsData}
+            />
+          );
         })}
+      </div>
+      <div className="my-10 flex flex-col">
+        <h1 className="text-2xl text-medium">Custom Prompt</h1>
+        <textarea
+          className="bg-slate-900 border-slate-400 border w-full max-w-[500px] rounded"
+          rows="10"
+          value={customPrompt}
+          onChange={handlePromptChange}
+        ></textarea>
+        <button onClick={handlePromptSave} className="bg-primary w-fit my-4 p-3 rounded">Save</button>
       </div>
       <CodeForm modalIsOpen={modalIsOpen} setIsOpen={setIsOpen} />
     </div>
   );
 };
-const RowItem = ({ data, index,setCouponsData }) => {
-  const [open, setOpen] = useState(false)
+const RowItem = ({ data, index, setCouponsData }) => {
+  const [open, setOpen] = useState(false);
   let codeUsed = 0;
   let requestCount = 0;
   data.codes.forEach((c) => {
@@ -132,22 +190,26 @@ const RowItem = ({ data, index,setCouponsData }) => {
     }
     requestCount = c.requestCount;
   });
-  const handleRevoke=async ()=>{
+  const handleRevoke = async () => {
     const res = await fetch(
-      import.meta.env.VITE_SERVER_URL + "/admin/"+(data.isRevoked ? "activate" : "revoke")+"/"+data.key
+      import.meta.env.VITE_SERVER_URL +
+        "/admin/" +
+        (data.isRevoked ? "activate" : "revoke") +
+        "/" +
+        data.key
     );
     const response = await res.json();
     if (response.success) {
       let dataArr = [];
       for (const key in response.data) {
-        dataArr.push({...response.data[key],key});
+        dataArr.push({ ...response.data[key], key });
       }
       setCouponsData(dataArr);
     }
-  }
-  const handleDownload=async ()=>{
-   await  downloadExcel(data.codes,data.managerName)
-  }
+  };
+  const handleDownload = async () => {
+    await downloadExcel(data.codes, data.managerName);
+  };
   return (
     <>
       <div className=" col-span-1 p-2">
@@ -166,21 +228,45 @@ const RowItem = ({ data, index,setCouponsData }) => {
         <h1>{requestCount}</h1>
       </div>
       <div className=" col-span-3 p-2">
-        <h1>{data.isRevoked  ?  <span className="bg-red-500 p-1 rounded">Revoked</span> : <span className="bg-green-500 p-1 rounded">Active</span> }</h1>
+        <h1>
+          {data.isRevoked ? (
+            <span className="bg-red-500 p-1 rounded">Revoked</span>
+          ) : (
+            <span className="bg-green-500 p-1 rounded">Active</span>
+          )}
+        </h1>
       </div>
       <div className=" col-span-3 p-2">
-        <h1>{data.date ? new Date(data.date).toDateString():new Date().toDateString()}</h1>
+        <h1>
+          {data.date
+            ? new Date(data.date).toDateString()
+            : new Date().toDateString()}
+        </h1>
       </div>
       <div className=" col-span-2 p-2">
-        <button className="relative"
-        onMouseEnter={()=>{setOpen(true)}}
-        onMouseLeave={()=>{setOpen(false)}}
+        <button
+          className="relative"
+          onMouseEnter={() => {
+            setOpen(true);
+          }}
+          onMouseLeave={() => {
+            setOpen(false);
+          }}
         >
           <img src="/images/three-dot.svg" alt="" />
-          {open && <div className="absolute left-full top-[50%] text-black bg-slate-300 z-10 rounded">
-            <button className="px-2 w-full py-2 border-b border-slate-500" onClick={handleRevoke} >{data.isRevoked ? "Activate": "Revoke"}</button>
-            <button className="px-2 w-full py-2"  onClick={handleDownload}>Download</button>
-          </div>}
+          {open && (
+            <div className="absolute left-full top-[50%] text-black bg-slate-300 z-10 rounded">
+              <button
+                className="px-2 w-full py-2 border-b border-slate-500"
+                onClick={handleRevoke}
+              >
+                {data.isRevoked ? "Activate" : "Revoke"}
+              </button>
+              <button className="px-2 w-full py-2" onClick={handleDownload}>
+                Download
+              </button>
+            </div>
+          )}
         </button>
       </div>
     </>
